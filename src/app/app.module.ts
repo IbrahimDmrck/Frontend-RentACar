f67546f0d8cac9990368c1165885acff726e5837
Ibrahim Demircik <ibrahimdmrck@gmail.com>
2022-08-23 17:22:41 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +51,8 @@ import { CarFilterPipepePipe } from './pipes/car-filter-pipepe.pipe';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({positionClass:"toast-top-right"})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/rentalDetailDto.ts
+++ src/app/models/rentalDetailDto.ts
@@ -1,6 +1,7 @@
 export interface RentalDetailDto{
+    id:number;
     customerName:string;
     brandName:string;
     rentDate:Date;
-    returnDate:Date;
+    returnDate?:Date;
 }
